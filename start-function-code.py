1936ccfeae070f68d62d4832ba0f9cab7fe9080b
Praveen Khobragade <117705143+Pravnk57@users.noreply.github.com>
2024-03-31 14:08:31 +0530

    Update start-function-code.py

---
+++ start-function-code.py
@@ -0,0 +1,20 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Initialize the EC2 client
+    ec2 = boto3.client('ec2')
+
+    # Specify the instance IDs of the EC2 instances you want to start
+    instance_ids = ['instance_id_1', 'instance_id_2']  # Add more instance IDs as needed
+
+    # Start the EC2 instances
+    response = ec2.start_instances(InstanceIds=instance_ids)
+
+    # Print the response
+    print(response)
+
+    # Optionally, you can return a response to indicate success or failure
+    return {
+        'statusCode': 200,
+        'body': 'EC2 instances started successfully.'
+    }
